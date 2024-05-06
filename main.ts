a5b4e6e767d47b9289aac5fc5ce09b75656efd07
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 23:18:58 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+//Task 14: Guest list: if you could invite anyone, living or deceased, to dinner, who would you invite?
+//make a list that includes at least three people you'd like to invite to dinner. then use your list to
+//print a message to each person, inviting them to dinner.
+
+let guestlist: string []= ["Kumail","Shahid","Tahami"];
+guestlist.map((items) => (console.log(`Dear ${items}, you are invited to dinner`)));
